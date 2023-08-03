585b7f7ca4f7b10ef63c79e7a831eb1a1c6b8d51
Matteo <matteo.courquin@fortuneo.com>
2023-08-04 00:27:15 +0200

    x contentful & :bug:

--- contentful/index.js
+++ contentful/index.js
@@ -1,9 +1,8 @@
-import { createClient } from 'contentful'
+import { createClient } from "contentful";
 
 const client = createClient({
   space: 'cse6m1hgf7gu',
 	accessToken: 'JpDWdG38G4U0BDSaarW_GX8-mqXc-Z3ab8byBrxmNWc'
 });
-
 
 export { client }
